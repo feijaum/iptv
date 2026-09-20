a9d44c46d56f5e0bf4729a8076e66a83f411e840
def joao_victor <jvleite7@gmail.com>
2026-09-20 06:55:52 -0300

    Add Cloudflare Worker for Brazilian M3U playlist

---
+++ worker.js
@@ -0,0 +1,20 @@
+export default {
+  async fetch() {
+    const url = "https://raw.githubusercontent.com/feijaum/iptv/main/br.m3u";
+    const r = await fetch(url);
+
+    if (!r.ok) {
+      return new Response("Playlist unavailable", { status: 502 });
+    }
+
+    return new Response(r.body, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/vnd.apple.mpegurl; charset=utf-8",
+        "Content-Disposition": "inline; filename=br.m3u",
+        "Cache-Control": "public, max-age=300",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+};
